d59b79f392073ceb3d98789a8b2d10e6852f6d66
MinhTrungDev-2004 <nmt.minhtrungdev@gmail.com>
2025-11-05 19:46:30 +0700

    student manager

--- lib/web/training_department/pages/training_department_home.dart
+++ lib/web/training_department/pages/training_department_home.dart
@@ -3,6 +3,7 @@ import 'teaching_schedule_page.dart';
 import 'teacher_management_page.dart';
 import 'classroom_management_page.dart';
 import 'subject_management_page.dart';
+import 'student_management_page.dart';
 
 const Color kPrimaryBlue = Color(0xFF19325B);
 const Color kCardBlue = Color(0xFF264D9D);
@@ -63,6 +64,8 @@ class _TrainingDepartmentHomeState extends State<TrainingDepartmentHome> {
         );
       case 3:
         return const TeacherManagementPage();
+      case 5:
+        return const StudentManagementPage();
       case 6:
         return const SubjectManagementPage();
       case 7:
